a565ee5bdf0cbae9de30b09341138b10fc3e6c1d
DonizeteVida <donizetevida1998@outlook.com>
2025-01-23 16:19:47 -0300

    Basic init window code

--- main.cpp
+++ main.cpp
@@ -3,7 +3,58 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+static void onFrameBufferSizeCallback(
+    GLFWwindow *window,
+    int width,
+    int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+static void onKeyCallback(
+    GLFWwindow *window,
+    int key,
+    int scancode,
+    int action,
+    int mods)
+{
+    std::cout << "Key callback: " << key << " action: " << action << std::endl;
+}
+
 int main(int, char **)
 {
-    std::cout << "Hello, from opengl-learn!\n";
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+    if (!window)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    glfwSetFramebufferSizeCallback(window, onFrameBufferSizeCallback);
+    glfwSetKeyCallback(window, onKeyCallback);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClearColor(.2f, .3f, .3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
+    return EXIT_SUCCESS;
 }
