cfcc46eceff91a376171ef8d1c943385b4e4f65e
DonizeteVida <donizetevida1998@outlook.com>
2025-01-23 17:30:50 -0300

    Vertex input

--- main.cpp
+++ main.cpp
@@ -46,6 +46,17 @@ int main(int, char **)
     glfwSetFramebufferSizeCallback(window, onFrameBufferSizeCallback);
     glfwSetKeyCallback(window, onKeyCallback);
 
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f
+    };
+
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
     while (!glfwWindowShouldClose(window))
     {
         glClearColor(.2f, .3f, .3f, 1.0f);
