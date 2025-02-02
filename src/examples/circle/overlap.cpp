17de8d2f733cee832418f6cf3f49e692e7e23e11
DonizeteVida <donizetevida1998@outlook.com>
2025-02-02 15:05:12 -0300

    Overlap circle based on square sample code

--- src/examples/circle/overlap.cpp
+++ src/examples/circle/overlap.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <functional>
+#include <vector>
+#include <cmath>
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
@@ -51,17 +53,34 @@ int main(int, char **)
     glfwSetFramebufferSizeCallback(window, onFrameBufferSizeCallback);
     glfwSetKeyCallback(window, onKeyCallback);
 
-    float vertices[] = {
-        .5f, .5f, .0f, // top right
-        .5f, -.5f, .0f, // bottom right
-        -.5f, -.5f, .0f, // bottom left
-        -.5f, .5f, .0f // top left
-    };
+    #define DIVISIONS 6
+    #define CIRCLE (2 * M_PI)
+    #define ANGLE (CIRCLE / DIVISIONS)
+    std::vector<float> vertices;
 
-    unsigned int indices[] = {
-        0, 1, 3, // first triangle
-        1, 2, 3 // second triangle
-    };
+    for (size_t division_n = 0; division_n < DIVISIONS; division_n++)
+    {
+        float start = division_n * ANGLE;
+        float end = (division_n + 1) * ANGLE;
+
+        center: {
+            vertices.push_back(.0f);
+            vertices.push_back(.0f);
+            vertices.push_back(.0f);
+        }
+
+        start: {
+            vertices.push_back(std::cos(start));
+            vertices.push_back(std::sin(start));
+            vertices.push_back(.0f);
+        }
+
+        end: {
+            vertices.push_back(std::cos(end));
+            vertices.push_back(std::sin(end));
+            vertices.push_back(.0f);
+        }
+    }
 
     std::function<void(int)> verifyShaderCompilationStatus = [](unsigned int id)
     {
@@ -69,7 +88,7 @@ int main(int, char **)
         glGetShaderiv(id, GL_COMPILE_STATUS, &status);
         if (!status)
         {
-            char infoLog[512] = { 0 };
+            char infoLog[512] = {0};
             glGetShaderInfoLog(id, sizeof(infoLog), nullptr, infoLog);
             std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED" << infoLog << std::endl;
         }
@@ -104,26 +123,21 @@ int main(int, char **)
     unsigned int arrayBufferObjectId;
     glGenBuffers(1, &arrayBufferObjectId);
     glBindBuffer(GL_ARRAY_BUFFER, arrayBufferObjectId);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    unsigned int elementArrayBufferObjectId;
-    glGenBuffers(1, &elementArrayBufferObjectId);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementArrayBufferObjectId);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
     glEnableVertexAttribArray(0);
 
-    #if 1
+#if 0
     glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    #endif
+#endif
 
     while (!glfwWindowShouldClose(window))
     {
         glClearColor(.2f, .3f, .3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(indices[0]), GL_UNSIGNED_INT, 0);
+        glDrawArrays(GL_TRIANGLES, 0, vertices.size() / 3);
 
         glfwPollEvents();
         glfwSwapBuffers(window);
